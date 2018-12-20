c95e18f0b563f67d5133b21392278f6722204a2d
aiden <s860620@gmail.com>
2018-12-21 01:02:55 +0900

    item25

---
+++ src/main/java/chapter4/item25/Dessert.java
@@ -0,0 +1,12 @@
+package chapter4.item25;
+
+// 톱 레벨 클래스
+/*
+class Utensil {
+    static final String NAME = "pot";
+}
+
+class Dessert {
+    static final String NAME = "pie";
+}
+*/
---
+++ src/main/java/chapter4/item25/Utensil.java
@@ -0,0 +1,12 @@
+package chapter4.item25;
+
+// 톱 레벨 클래스
+/*
+class Utensil {
+    static final String NAME = "pan";
+}
+
+class Dessert {
+    static final String NAME = "cake";
+}
+*/
