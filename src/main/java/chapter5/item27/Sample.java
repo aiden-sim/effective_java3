4c71be247d2626046126f846b3973a832d67e5f1
aiden <s860620@gmail.com>
2018-12-24 12:44:40 +0900

    item27

---
+++ src/main/java/chapter5/item27/Sample.java
@@ -0,0 +1,11 @@
+package chapter5.item27;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Sample {
+    public static void main(String[] args) {
+        @SuppressWarnings("unchecked")
+        Set<String> set = new HashSet();
+    }
+}
