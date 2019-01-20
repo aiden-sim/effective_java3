215efcae916d7830f94de7a5a02a26112052da73
aiden <s860620@gmail.com>
2019-01-20 23:11:24 +0900

    item59

---
+++ src/main/java/chapter8/item59/Sample.java
@@ -0,0 +1,24 @@
+package chapter8.item59;
+
+import java.util.Random;
+
+public class Sample {
+
+    static Random rnd = new Random();
+
+    static int random(int n) {
+        return Math.abs(rnd.nextInt()) % n;
+    }
+
+    public static void main(String[] args) {
+        int n = 2 * (Integer.MAX_VALUE / 3);
+        int low = 0;
+        for (int i = 0; i < 1000000; i++) {
+            if (random(n) < n / 2) {
+                low++;
+            }
+        }
+
+        System.out.println(low);
+    }
+}
