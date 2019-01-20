de24cf6e341470e985c1c92afe07cbb0d766031c
aiden <s860620@gmail.com>
2019-01-20 20:42:20 +0900

    item57

--- src/main/java/chapter8/item57/Sample.java
+++ src/main/java/chapter8/item57/Sample.java
@@ -1,5 +1,7 @@
 package chapter8.item57;
 
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 public class Sample {
@@ -10,6 +12,19 @@ public class Sample {
                     Class.forName(args[0]);
         } catch (ClassNotFoundException e) {
             System.out.println("클래스를 찾을 없습니다.");
+        }
+
+        List<String> list = List.of("test", "tset2", "test3");
+        List<String> list2 = List.of("test", "tset2", "test3");
+
+        Iterator<String> i = list.iterator();
+        while (i.hasNext()) {
+
+        }
+
+        Iterator<String> i2 = list2.iterator();
+        while(i.hasNext()) { // 버그 i를 사용했다.
+
         }
     }
 }
