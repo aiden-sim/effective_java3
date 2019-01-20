6cdbd1082fbbe6c0dc6e47bb970e7e101d434e02
aiden <s860620@gmail.com>
2019-01-21 01:07:20 +0900

    item61

---
+++ src/main/java/chapter8/item61/Unbelievable.java
@@ -0,0 +1,11 @@
+package chapter8.item61;
+
+public class Unbelievable {
+    static Integer i;
+
+    public static void main(String[] args) {
+        if (i == 42) {
+            System.out.println("믿을 수 없군!");
+        }
+    }
+}
