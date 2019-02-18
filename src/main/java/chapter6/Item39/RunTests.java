172a563576aee32666842db271f63d06878f224f
aiden <s860620@gmail.com>
2019-02-19 01:27:05 +0900

    item39

--- src/main/java/chapter6/Item39/RunTests.java
+++ src/main/java/chapter6/Item39/RunTests.java
@@ -2,13 +2,14 @@ package chapter6.Item39;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class RunTests {
     public static void main(String[] args) throws Exception {
+        // 마커 애너테이션을 처리하는 프로그램
         int tests = 0;
         int passed = 0;
         Class<?> testClass = Class.forName("chapter6.Item39.Sample");
-
         for (Method m : testClass.getDeclaredMethods()) {
             if (m.isAnnotationPresent(Test.class)) {
                 tests++;
@@ -25,12 +26,62 @@ public class RunTests {
         }
         System.out.printf("성공: %d, 실패: %d%n", passed, tests - passed);
 
+        // 매개변수 하나짜리 애너테이션을 사용한 프로그램
         tests = 0;
         passed = 0;
         testClass = Class.forName("chapter6.Item39.Sample2");
-
         for (Method m : testClass.getDeclaredMethods()) {
-            if (m.isAnnotationPresent(ExceptionTest.class) || m.isAnnotationPresent(ExceptionTestContainer.class)) {
+            if (m.isAnnotationPresent(ExceptionTest.class)) {
+                tests++;
+                try {
+                    m.invoke(null);
+                    System.out.printf("테스트 %s 실패: 예외를 던지지 않음%n", m);
+                } catch (InvocationTargetException wrappedExc) {
+                    Throwable exc = wrappedExc.getCause();
+                    Class<? extends Throwable> excType = m.getAnnotation(ExceptionTest.class).value();
+                    if (excType.isInstance(exc)) {
+                        passed++;
+                    } else {
+                        System.out.printf("테스트 %s 실패: 기대한 예외 %s, 발생한 예외 %s%n", m, excType.getName(), exc);
+                    }
+                } catch (Exception e) {
+                    System.out.println("잘못 사용한 @ExceptionTest: " + m);
+                }
+            }
+        }
+
+        // 배열 매개변수를 받는 애너테이션 코드
+        tests = 0;
+        passed = 0;
+        testClass = Class.forName("chapter6.Item39.Sample2");
+        for (Method m : testClass.getDeclaredMethods()) {
+            if (m.isAnnotationPresent(ExceptionTest2.class)) {
+                tests++;
+                try {
+                    m.invoke(null);
+                    System.out.printf("테스트 %s 실패: 예외를 던지지 않음%n", m);
+                } catch (InvocationTargetException wrappedExc) {
+                    Throwable exc = wrappedExc.getCause();
+                    int oldPassed = passed;
+                    Class<? extends Throwable>[] excTypes = m.getAnnotation(ExceptionTest2.class).value();
+                    for (Class<? extends Throwable> excType : excTypes) {
+                        if (excType.isInstance(exc)) {
+                            passed++;
+                            break;
+                        }
+                    }
+                    if (passed == oldPassed) {
+                        System.out.printf("테스트 %s 실패: %s %n", m, exc);
+                    }
+                }
+            }
+        }
+
+        // 반복 가능 애너테이션 다루기
+        for (Method m : testClass.getDeclaredMethods()) {
+            ExceptionTest3[] test = m.getAnnotationsByType(ExceptionTest3.class);
+            System.out.println(Arrays.asList(test));
+            if (m.isAnnotationPresent(ExceptionTest3.class) || m.isAnnotationPresent(ExceptionTestContainer.class)) {
                 tests++;
                 try {
                     m.invoke(null);
@@ -38,10 +89,10 @@ public class RunTests {
                 } catch (Throwable wrappedExc) {
                     Throwable exc = wrappedExc.getCause();
                     int oldPassed = passed;
-                    ExceptionTest[] excTypes =
-                            m.getAnnotationsByType(ExceptionTest.class);
+                    ExceptionTest3[] excTypes =
+                            m.getAnnotationsByType(ExceptionTest3.class);
 
-                    for (ExceptionTest excTest : excTypes) {
+                    for (ExceptionTest3 excTest : excTypes) {
                         if (excTest.value().isInstance(exc)) {
                             passed++;
                             break;
