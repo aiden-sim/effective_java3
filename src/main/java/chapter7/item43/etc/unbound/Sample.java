76fd1c61385662399e07d67f5dd497a3db6a71a7
simjunbo <s860620@gmail.com>
2019-03-05 16:51:15 +0900

    unbound

---
+++ src/main/java/chapter7/item43/etc/unbound/Sample.java
@@ -0,0 +1,40 @@
+package chapter7.item43.etc.unbound;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class Sample {
+	public static void main(String[] args) {
+		List<Child> list = new ArrayList<>();
+		list.add(new Child("test", 1));
+
+		Function<Child, String> function = (a) -> a.getName();
+		Function<Child, String> function2 = Child::getName;
+
+		String name = list.stream()
+				.map(function2)
+				.findAny().get();
+
+		System.out.println(name);
+	}
+
+	private static class Child {
+		private String name;
+
+		private int age;
+
+		public Child(String name, int age) {
+			this.name = name;
+			this.age = age;
+		}
+
+		public int getAge() {
+			return age;
+		}
+
+		public String getName() {
+			return name;
+		}
+	}
+}
--- src/main/java/chapter7/item43/etc/unbound/UnBoundType.java
+++ src/main/java/chapter7/item43/etc/unbound/UnBoundType.java
@@ -1,12 +1,17 @@
 package chapter7.item43.etc.unbound;
 
 import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
 
 public class UnBoundType {
 
 	// instance method
 	public int cubic(int a) {
 		return a * a * a;
+	}
+
+	public boolean testMethod(boolean falg) {
+		return falg;
 	}
 
 	public static void main(String[] args) {
@@ -19,5 +24,8 @@ public class UnBoundType {
 		// Using a method reference (UnBound type)
 		BiFunction<UnBoundType, Integer, Integer> operator2 = UnBoundType::cubic;
 		System.out.println(operator2.apply(cal, 6));
+
+		BiPredicate<UnBoundType, Boolean> predicate = UnBoundType::testMethod;
+		System.out.println(predicate.test(cal, true));
 	}
 }
